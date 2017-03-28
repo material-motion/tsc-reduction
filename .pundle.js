65c7294bd8463dbb2d2b1b1616b1826033309545
Brenton Simpson <appsforartists@google.com>
2017-03-28 13:22:46 -0700

    Initial commit.

---
+++ .pundle.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const tsConfig = require('./tsconfig');
+
+const extensions = ['js', 'jsx', 'ts', 'tsx'];
+
+module.exports = {
+  debug: true,
+  rootDirectory: __dirname,
+  entry: ['./src/index.ts'],
+  output: {
+    rootDirectory: './site/dist',
+    bundlePath: 'bundle.js',
+    sourceMap: true,
+    sourceMapPath: 'bundle.js.map',
+  },
+  server: {
+    port: 8080,
+    rootDirectory: './site',
+    hmrPath: '/dist/bundle_hmr',
+    bundlePath: '/dist/bundle.js',
+    sourceMapPath: '/dist/bundle.js.map',
+    redirectNotFoundToIndex: true,
+  },
+  presets: [
+    [
+      require.resolve('pundle-preset-default'),
+      {
+        resolver: false,
+        loader: false,
+      }
+    ],
+    [
+      require.resolve('pundle-preset-typescript'),
+      {
+        loader: {
+          extensions,
+        },
+        resolver: {
+          packageMains: ['typescript:main', 'module', 'browser', 'main'],
+          extensions,
+        },
+        transformer: {
+          extensions,
+          config: {
+            compilerOptions: Object.assign(
+              {},
+              // mainTSConfig.compilerOptions,
+              tsConfig.compilerOptions
+            )
+          }
+        }
+      }
+    ],
+  ],
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is simply a reduction to help the TypeScript team investigate https://github.com/Microsoft/TypeScript/issues/14890.  Ignore it.
